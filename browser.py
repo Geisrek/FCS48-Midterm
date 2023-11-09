88167da1142301940f93ca2b58e8dd9ea6df99e4
Rafik Hijazy <rafikhz90@gmail.com>
2023-11-09 23:00:56 +0200

    commit 4

--- browser.py
+++ browser.py
@@ -4,4 +4,19 @@ Created on Thu Nov  9 20:39:35 2023
 
 @author: USER
 """
-
+import json
+class Browser:
+    def __init__(self):
+        self
+    def jsonReader(self,url):
+        output={}
+        try:
+            with open(url,'r') as file:
+                output=json.load(file)
+        except Exception as e:
+            output={'Errore':e}
+        return output
+    def openTab(self,title,url):
+        pass
+browser=Browser()
+print(type(browser.jsonReader('./web/Youtube.json')))
--- main.py
+++ main.py
@@ -6,6 +6,38 @@ Created on Thu Nov  9 20:05:04 2023
 """
 
 def main():
-    options="1. Open Tab\n2. Close Tab\n3. Switch Tab\n4. Display All Tabs\n5. Open Nested Tab\n6. Clear All Tabs\n7. Save Tabs\n8. Import Tabs\n9. Exit"
-    print(options)
+    text="1. Open Tab\n2. Close Tab\n3. Switch Tab\n4. Display All Tabs\n5. Open Nested Tab\n6. Clear All Tabs\n7. Save Tabs\n8. Import Tabs\n9. Exit"
+    option=int(input(f"{text}:"))
+    if option==1:
+        title=input("Enter the title:")
+        URL=input("Enter the URL:")
+        ######
+        main()
+    elif option==2:
+        ######
+        main()
+    elif option==3:
+        ######
+        main()
+    elif option==4:
+        ######
+        main()
+    elif option==5:
+        ######
+        main()
+    elif option==6:
+        ######
+        main()
+    elif option==7:
+        ######
+        main()
+    elif option==8:
+        ######
+        main()
+    elif option==9:
+        return
+    else:
+        print("Unvalid operation please make sure you input a digit in range 0 to 9")
+        main()
+    
 main()
