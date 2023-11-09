7df173071bac6cbb60373d0316c60cc7595037e3
Rafik Hijazy <rafikhz90@gmail.com>
2023-11-09 20:39:04 +0200

    commit 1

---
+++ main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  9 20:05:04 2023
+
+@author: USER
+"""
+
+def main():
+    options="1. Open Tab\n2. Close Tab\n3. Switch Tab\n4. Display All Tabs\n5. Open Nested Tab\n6. Clear All Tabs\n7. Save Tabs\n8. Import Tabs\n9. Exit"
+    print(options)
+main()
